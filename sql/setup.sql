7a12c189eb7abf533fb985949a12b9b17bd0fa33
Mary Martinez <mary.diana.martinez@gmail.com>
2022-07-08 14:51:36 -0700

    oauth get routes are working

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS github_users;
+
+CREATE TABLE github_users(
+  if BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  username VARCHAR NOT NULL,
+  email VARCHAR,
+  avatar VARCHAR
+);
