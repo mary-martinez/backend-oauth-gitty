021db1b4b3113f1fc8d89c8575aa95b606db7144
Mary Martinez <mary.diana.martinez@gmail.com>
2022-07-08 21:27:53 -0700

    testing for oauth

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,32 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/services/github');
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('should redirect to the github oauth page when logging in', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
+  });
+  it('should login a user and redirect to /api/v1/github/dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'mock_github_user',
+      avatar: 'https://www.placecage.com/gif/300/300',
+      email: 'mock-me@aol.com',
+      iat: expect.any(Number),
+      exp: expect.any(Number)
+    });
   });
   afterAll(() => {
     pool.end();
